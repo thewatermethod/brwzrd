f8d9867fc54c958bcd7140d48e1d95b6aaa9e2a4
Matt Bevilacqua <mbevilac@providence.edu>
2020-09-04 13:17:30 -0400

    Removed env, added required packages

--- functions/handleImageUpload/handleImageUpload.js
+++ functions/handleImageUpload/handleImageUpload.js
@@ -1,4 +1,17 @@
+"use strict";
+const express = require("express");
+const serverless = require("serverless-http");
+const app = express();
+const bodyParser = require("body-parser");
 const cloudinary = require("cloudinary").v2;
+
+app.use(bodyParser);
+app.post("/updatestate", (req, res) => {
+  const newValue = updateDatabase(res.body);
+  res.json(newValue);
+});
+
+module.exports.handler = serverless(app);
 
 // cloudinary.config({
 //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,

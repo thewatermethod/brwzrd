b963e8af1e09988e1125d741c9a5282030b12857
Matt Bevilacqua <mbevilac@providence.edu>
2020-09-04 11:47:18 -0400

    lets try this

--- functions/handleImageUpload/handleImageUpload.js
+++ functions/handleImageUpload/handleImageUpload.js
@@ -1,10 +1,10 @@
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
@@ -19,8 +19,8 @@ exports.handler = async (event, context) => {
       return {
         statusCode: 200,
         body: JSON.stringify({message: "Function invoked"}),
-      };
-    });
+      }
+    }
   } catch (err) {
     return {statusCode: 500, body: err.toString()};
   }

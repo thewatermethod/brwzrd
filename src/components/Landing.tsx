4f0912f677607f2620a48ef486c895f00f81d7af
Matt Bevilacqua <mbevilac@providence.edu>
2020-09-16 11:19:16 -0400

    create landing page content

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,9 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
+import {ApolloClient, createHttpLink, InMemoryCache} from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
+import {gql} from "@apollo/client";
 
+import Preview, {PreviewProps} from "./Preview";
 import "../css/landing.css";
 
 const Landing: React.SFC<{darkMode: boolean}> = (props) => {
-  return <main className="landing-main"></main>;
+  const [previews, setPreviews] = useState<PreviewProps[] | false>(false);
+
+  if (!previews) {
+    const httpLink = createHttpLink({
+      uri: "https://graphql.fauna.com/graphql",
+    });
+
+    const authLink = setContext((_, {headers}) => {
+      return {
+        headers: {
+          ...headers,
+          authorization: `Bearer ${process.env.REACT_APP_FAUNA}`,
+        },
+      };
+    });
+
+    const client = new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache(),
+    });
+
+    client
+      .query({
+        query: gql`
+          query {
+            LastThreeBrews(_size: 3) {
+              data {
+                _id
+                name
+                image
+                style
+              }
+            }
+          }
+        `,
+      })
+      .then((result) => {
+        //console.log(result.data.LastThreeBrews.data);
+        setPreviews(result.data.LastThreeBrews.data);
+      });
+  }
+
+  return (
+    <main className="landing-main">
+      <h2>Recent Brews</h2>
+      <div className="previews">
+        {previews
+          ? previews.map((preview: PreviewProps) => {
+              return (
+                <Preview
+                  key={preview._id}
+                  name={preview.name}
+                  _id={preview._id}
+                  image={preview.image}
+                  style={preview.style}
+                />
+              );
+            })
+          : null}
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
---
+++ src/components/Preview.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import CSS from "csstype";
+
+import "../css/preview.css";
+
+export interface PreviewProps {
+  _id: string;
+  name: string;
+  image: string;
+  style: string;
+}
+
+const Preview: React.SFC<PreviewProps> = (props) => {
+  let previewClass = "preview";
+  const stylings: CSS.Properties = {
+    backgroundColor: "lightsteelblue",
+  };
+
+  if (props.image != "") {
+    stylings.backgroundImage = `url(${props.image})`;
+    previewClass = "preview has-image";
+  }
+
+  return (
+    <article className={previewClass} style={stylings}>
+      <Link to={`/brew/${props._id}`}>
+        <h3>{props.name}</h3>
+      </Link>
+      <p>{props.style}</p>
+    </article>
+  );
+};
+
+export default Preview;
